7b1b1c2889995e4ff6bf8d91d5f13635f85b4f2b
KKoder1991 <konondlovu@icloud.com>
2021-03-26 17:05:55 +0100

    Defined const and let variables for the project.

---
+++ counter-logic.js
@@ -0,0 +1,6 @@
+// Variable to save the count to when counting up or down.
+let count = 0;
+
+// Select the different parts of the DOM.
+const counter_span = document.querySelector("#value");
+const buttons = document.querySelector("buttons");
